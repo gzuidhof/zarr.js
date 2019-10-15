345ddf3c4cba644562939d31b5866ba369b80f3d
Guido Zuidhof <me@guido.io>
2019-10-15 01:05:23 +0100

    Clean tests, fix publish script

--- test/types.test.ts
+++ test/types.test.ts
@@ -1,19 +1,17 @@
-import * as NAMES from "../src/names";
-import * as fs from "fs";
+import { readFileSync } from "fs";
 import * as path from "path";
-
-import typesTI from "../src/types-ti";
 import { createCheckers } from "ts-interface-checker";
+import * as NAMES from "../src/names";
+import typesTI from "../src/types-ti";
+
+const FIXTURES_FOLDER = path.resolve(__dirname, `../fixtures`);
 const { ZarrMetadata } = createCheckers(typesTI);
 
-const FIXTURES_FOLDER = path.resolve(__dirname, `../fixtures`)
-
 describe("ZarrMetadata Typings", () => {
-
-    const metadataPath = path.join(FIXTURES_FOLDER, `empty.zarr/${NAMES.METADATA_FILENAME}`)
-    const simpleMetadata = JSON.parse(fs.readFileSync(metadataPath, { encoding: "utf8" }));
+    const metadataPath = path.join(FIXTURES_FOLDER, `empty.zarr/${NAMES.METADATA_FILENAME}`);
+    const simpleMetadata = JSON.parse(readFileSync(metadataPath, { encoding: "utf8" }));
 
     it("Simple ZarrMetadata is valid", () => {
-        expect(ZarrMetadata.check(simpleMetadata)).toBeUndefined();
-    })
+        expect(ZarrMetadata.test(simpleMetadata)).toBeTruthy();
+    });
 })
--- tools/gh-pages-publish.ts
+++ tools/gh-pages-publish.ts
@@ -26,6 +26,6 @@ exec('git config user.name "Guido Zuidhof"')
 exec('git config user.email "me@guido.io"')
 exec('git commit -m "docs(docs): update gh-pages"')
 exec(
-  `git push --force --quiet "https://${ghToken}@${repository}" master:gh-pages`
+  `git push --force --quiet "git@github.com:gzuidhof/zarr.js.git" master:gh-pages`
 )
 echo("Docs deployed!!")
