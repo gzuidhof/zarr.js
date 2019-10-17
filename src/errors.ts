8b91e8b71828d96154254802f555b4c2b4ea58c0
Guido Zuidhof <me@guido.io>
2019-10-18 00:31:02 +0100

    Add errors

---
+++ src/errors.ts
@@ -0,0 +1,29 @@
+export class ContainsArrayError extends Error {
+    constructor(path: string) {
+        super(`path ${path} contains an array`);
+    }
+}
+
+export class ContainsGroupError extends Error {
+    constructor(path: string) {
+        super(`path ${path} contains a group`);
+    }
+}
+
+export class ArrayNotFoundError extends Error {
+    constructor(path: string) {
+        super(`array not found at path ${path}`);
+    }
+}
+
+export class GroupNotFoundError extends Error {
+    constructor(path: string) {
+        super(`ground not found at path ${path}`);
+    }
+}
+
+export class PathNotFoundError extends Error {
+    constructor(path: string) {
+        super(`nothing not found at path ${path}`);
+    }
+}
