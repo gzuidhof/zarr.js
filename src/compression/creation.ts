4206f9812264514d02ca5b9923c93166d4b7a125
Guido Zuidhof <me@guido.io>
2019-12-10 00:34:06 +0000

    Make the compiler happy

--- src/compression/creation.ts
+++ src/compression/creation.ts
@@ -3,11 +3,11 @@ import { GZip } from "./gzip";
 import { Zlib } from "./zlib";
 import { Codec } from "./types";
 
-export function getCodec(config: CompressorConfig & any): Codec {
+export function getCodec(config: CompressorConfig & {level: number}): Codec {
     if (config.id === GZip.codecId) {
-        return new GZip((config as any)["level"]);
+        return new GZip(config.level);
     } else if (config.id === Zlib.codecId) {
-        return new Zlib((config as any)["level"]);
+        return new Zlib(config.level);
     } else {
         throw new Error(`Compression codec ${config.id} is not supported by Zarr.js yet.`);
     }
