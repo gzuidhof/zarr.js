aa1330b15498fc925002c36a27ff6fa9f48ebb71
Guido Zuidhof <me@guido.io>
2020-01-10 00:45:05 +0000

    Don't use buffer in browser

--- src/storage/httpStore.ts
+++ src/storage/httpStore.ts
@@ -1,41 +1,47 @@
-import { SyncStore, ValidStoreType, AsyncStore } from "./types";
-import { createProxy, MutableMappingProxy } from "../mutableMapping";
-import { KeyError } from "../errors";
+import { SyncStore, ValidStoreType, AsyncStore } from './types'
+import { createProxy, MutableMappingProxy } from '../mutableMapping'
+import { KeyError } from '../errors'
 
 export class HTTPStore implements AsyncStore<ArrayBuffer> {
-    listDir?: undefined;
-    rmDir?: undefined;
-    getSize?: undefined;
-    rename?: undefined;
+  listDir?: undefined
+  rmDir?: undefined
+  getSize?: undefined
+  rename?: undefined
 
-    public url: string;
+  public url: string
 
-    constructor(url: string) {
-        this.url = url;
-    }
+  constructor(url: string) {
+    this.url = url
+  }
 
-    keys(): Promise<string[]> {
-        throw new Error("Method not implemented.");
-    }
-    async getItem(item: string) {
-        const url = new URL(item, this.url).href;
-        const value = await fetch(url);
-        return Buffer.from(await value.arrayBuffer());
-    }
-    async setItem(item: string, value: ValidStoreType): Promise<boolean> {
-        if (typeof value === "string") {
-            value = new TextEncoder().encode(value).buffer;
-        }
-        const set = await fetch(item, { method: "PUT" });
-        return set.status.toString()[0] === "2";
-    }
-    deleteItem(item: string): Promise<boolean> {
-        throw new Error("Method not implemented.");
-    }
-    async containsItem(item: string): Promise<boolean> {
-        const url = new URL(item, this.url).href;
-        const value = await fetch(url);
+  keys(): Promise<string[]> {
+    throw new Error('Method not implemented.')
+  }
 
-        return value.status === 200;
+  async getItem(item: string) {
+    const url = new URL(item, this.url).href
+    const value = await fetch(url)
+    if (typeof window === 'undefined') {
+      // Node
+      return Buffer.from(await value.arrayBuffer())
     }
+    return await value.arrayBuffer() // Browser
+  }
+
+  async setItem(item: string, value: ValidStoreType): Promise<boolean> {
+    if (typeof value === 'string') {
+      value = new TextEncoder().encode(value).buffer
+    }
+    const set = await fetch(item, { method: 'PUT' })
+    return set.status.toString()[0] === '2'
+  }
+  deleteItem(item: string): Promise<boolean> {
+    throw new Error('Method not implemented.')
+  }
+  async containsItem(item: string): Promise<boolean> {
+    const url = new URL(item, this.url).href
+    const value = await fetch(url)
+
+    return value.status === 200
+  }
 }
