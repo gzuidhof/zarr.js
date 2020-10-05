dd2e0c0b573467250523e18811f1b6f08f591ef8
Trevor Manz <trevor.j.manz@gmail.com>
2020-10-05 10:15:29 -0400

    use top level exports and refactor rollup.config

--- rollup.config.ts
+++ rollup.config.ts
@@ -1,49 +1,57 @@
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
-import json from '@rollup/plugin-json';
-import sourceMaps from 'rollup-plugin-sourcemaps';
 import typescript from 'rollup-plugin-typescript2';
+import bundleSize from 'rollup-plugin-bundle-size';
+import { terser } from 'rollup-plugin-terser';
 
-const pkg = require('./package.json');
-
-const libraryName = 'zarr';
-
-const config = ({ input, output }) => {
-  return {
-    input,
-    output,
+export default [
+  {
+    input: {
+      zarr: 'src/zarr.ts',
+      core: 'src/zarr-core.ts',
+    },
+    output: [
+      {
+        dir: 'dist/',
+        format: 'es',
+        entryFileNames: '[name].mjs',
+        sourcemap: true,
+      },
+      {
+        dir: 'dist/',
+        format: 'cjs',
+        entryFileNames: '[name].cjs',
+        sourcemap: true,
+      },
+    ],
     watch: {
       include: 'src/**',
     },
     plugins: [
-      // Allow json resolution
-      json(),
-      // Compile TypeScript files
       typescript({ useTsconfigDeclarationDir: true }),
-      // Allow bundling cjs modules (unlike webpack, rollup doesn't understand cjs)
       commonjs(),
-      // Allow node_modules resolution, so you can use 'external' to control
-      // which external modules to include in the bundle
-      // https://github.com/rollup/rollup-plugin-node-resolve#usage
       resolve(),
-      // Resolve source maps to the original source
-      sourceMaps(),
+    ],
+  },
+  {
+    input: 'src/zarr.ts',
+    output: {
+      file: 'dist/zarr.umd.js',
+      name: 'zarr',
+      format: 'umd',
+      sourcemap: true,
+      esModule: false,
+    },
+    plugins: [
+      typescript({
+        useTsconfigDeclarationDir: true,
+        // https://github.com/ezolenko/rollup-plugin-typescript2/issues/105
+        objectHashIgnoreUnknownHack: true,
+      }),
+      commonjs(),
+      resolve(),
+      terser(),
+      bundleSize(),
     ],
   }
-}
-
-export default [
-  config({
-    input: `src/${libraryName}.ts`,
-    output: [
-      { file: pkg.main, name: libraryName, format: 'umd', sourcemap: true },
-      { file: pkg.module, format: 'es', sourcemap: true },
-    ],
-  }),
-  config({
-    input: "src/zarr-core.ts",
-    output: {
-      file: pkg.exports['./core'], format: 'es', sourcemap: true
-    }
-  })
 ];
