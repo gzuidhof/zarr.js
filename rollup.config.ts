1fbe8d428c95ed6e47769a5b1837063071f8d57d
Trevor Manz <trevor.j.manz@gmail.com>
2021-03-02 13:38:30 -0500

    fix rollup; use manualChunking

--- rollup.config.ts
+++ rollup.config.ts
@@ -6,22 +6,23 @@ import visualizer from 'rollup-plugin-visualizer';
 
 const commonPlugins = () => [typescript({ useTsconfigDeclarationDir: true }), commonjs(), resolve()];
 
-
 export default [
   {
-    input: { zarr: 'src/zarr.ts', core: 'src/zarr-core.ts' },
+    input: 'src/zarr.ts',
     output: [{
       dir: 'dist/',
       format: 'es',
       entryFileNames: '[name].mjs',
-      chunkFileNames: '[name]-[hash].mjs',
+      chunkFileNames: '[name].mjs',
+      manualChunks: { core: ['src/zarr-core.ts'] },
       sourcemap: true,
     },
     {
       dir: 'dist/',
       format: 'es',
       entryFileNames: '[name].min.mjs',
-      chunkFileNames: '[name]-[hash].min.mjs',
+      chunkFileNames: '[name].min.mjs',
+      manualChunks: { core: ['src/zarr-core.ts'] },
       sourcemap: true,
       plugins: [terser()]
     },
@@ -29,7 +30,11 @@ export default [
     watch: {
       include: 'src/**',
     },
-    plugins: [...commonPlugins(), visualizer({ filename: "stats.html" }), visualizer({ filename: "stats.min.html", sourcemap: true })],
+    plugins: [
+      ...commonPlugins(),
+      visualizer({ filename: "stats.html" }),
+      visualizer({ filename: "stats.min.html", sourcemap: true })
+    ],
   },
   {
     input: 'src/zarr.ts',
