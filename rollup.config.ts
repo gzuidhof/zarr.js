7fe465280358bee4893b1995b910f51bd920fa8d
Trevor Manz <trevor.j.manz@gmail.com>
2021-03-01 19:12:17 -0500

    Upgrade rollup & deps

--- rollup.config.ts
+++ rollup.config.ts
@@ -4,7 +4,7 @@ import typescript from 'rollup-plugin-typescript2';
 import { terser } from 'rollup-plugin-terser';
 import visualizer from 'rollup-plugin-visualizer';
 
-const commonPlugins = () => [typescript({ useTsconfigDeclarationDir: true, objectHashIgnoreUnknownHack: true, }), commonjs(), resolve()]; 
+const commonPlugins = () => [typescript({ useTsconfigDeclarationDir: true }), commonjs(), resolve()];
 
 
 export default [
@@ -23,11 +23,11 @@ export default [
       sourcemap: true,
       plugins: [terser()]
     },
-  ],
+    ],
     watch: {
       include: 'src/**',
     },
-    plugins: [...commonPlugins(), visualizer({filename: "stats.html"}), visualizer({filename: "stats.min.html", sourcemap: true})],
+    plugins: [...commonPlugins(), visualizer({ filename: "stats.html" }), visualizer({ filename: "stats.min.html", sourcemap: true })],
   },
   {
     input: 'src/zarr.ts',
