f7d6186fd9d9178b6ce2afb5a38a3a2298e25530
Trevor Manz <trevor.j.manz@gmail.com>
2021-04-08 11:54:21 -0400

    Create nested fixture

--- fixtures/generate_fixtures.py
+++ fixtures/generate_fixtures.py
@@ -1,8 +1,11 @@
-import zarr
 import os
 
-FIXTURES_FOLDER = "./"
+import zarr  # 2.7
+from zarr.convenience import copy_store
+from zarr.storage import DirectoryStore
+from zarr.util import json_dumps, json_loads
 
+FIXTURES_FOLDER = "./"
 
 def generate_fixtures():
 
@@ -50,6 +53,36 @@ def generate_fixtures():
         simple_zarr_array[0, 0] = 1
         simple_zarr_array[0, 1] = 2
         simple_zarr_array[7, 7] = 3
+
+    # nested
+    store = dict()
+    nested = zarr.open(
+        store,
+        shape=(8, 8),
+        chunks=(2, None),
+        dtype=">i4",
+        fill_value=0,
+        mode="w",
+    )
+    nested[0, 0] = 1
+    nested[0, 1] = 2
+    nested[7, 7] = 3
+
+    # manually convert store
+    nested_store = dict()
+    for key, value in store.items():
+        if key == ".zarray":
+            # add missing field to .zarry metadata
+            value = json_loads(value)
+            value['dimension_separator'] = '/'
+            value = json_dumps(value)
+        else:
+            # map keys from "Y.X" -> "Y/X"
+            key = key.replace(".", "/")
+        nested_store[key] = value
+
+    # copy dict to directory
+    copy_store(nested_store, DirectoryStore(os.path.join(FIXTURES_FOLDER, 'simple_nested.zarr')))
 
 
 if __name__ == "__main__":
