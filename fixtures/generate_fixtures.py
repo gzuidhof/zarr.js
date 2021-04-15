3ed3e8b3c0f6f7123d7907eb220e07534f6161ca
Trevor Manz <trevor.j.manz@gmail.com>
2021-04-15 13:14:47 -0400

    Extract method to create simple fixure

--- fixtures/generate_fixtures.py
+++ fixtures/generate_fixtures.py
@@ -1,59 +1,56 @@
 import os
 
-import zarr  # 2.7
+import zarr
+from zarr.creation import create  # 2.7
 from zarr.util import json_dumps, json_loads
 
 FIXTURES_FOLDER = "./"
 
 
-def generate_fixtures():
-
-    empty_path = os.path.join(FIXTURES_FOLDER, "empty.zarr")
-    zarr.open(
-        empty_path,
+def create_simple_array(store, dtype, compression=None, write_chunks=False):
+    arr = zarr.open(
+        store=store,
         shape=(8, 8),
         chunks=(2, None),
-        dtype="<i4",
+        dtype=dtype,
         fill_value=0,
         mode="w",
-        compressor=None,
+        compression=compression,
     )
+    if write_chunks:
+        arr[0, 0] = 1
+        arr[0, 1] = 2
+        arr[7, 7] = 3
+
+
+def generate_fixtures():
+
+    # Empty fixture
+    create_simple_array(os.path.join(FIXTURES_FOLDER, "empty.zarr"), dtype="<i4")
 
     # little endian
     for codec in (None, "gzip", "zlib"):
-        simple_path = os.path.join(
+        path = os.path.join(
             FIXTURES_FOLDER, "simple{}_LE.zarr".format(f"_{codec}" if codec else "")
         )
-        simple_zarr_array = zarr.open(
-            simple_path,
-            shape=(8, 8),
-            chunks=(2, None),
+        create_simple_array(
+            store=path,
             dtype="<i4",
-            fill_value=0,
-            mode="w",
             compression=codec,
+            write_chunks=True,
         )
-        simple_zarr_array[0, 0] = 1
-        simple_zarr_array[0, 1] = 2
-        simple_zarr_array[7, 7] = 3
 
     # big endian
     for codec in (None, "gzip", "zlib"):
-        simple_path = os.path.join(
+        path = os.path.join(
             FIXTURES_FOLDER, "simple{}_BE.zarr".format(f"_{codec}" if codec else "")
         )
-        simple_zarr_array = zarr.open(
-            simple_path,
-            shape=(8, 8),
-            chunks=(2, None),
+        create_simple_array(
+            store=path,
             dtype=">i4",
-            fill_value=0,
-            mode="w",
             compression=codec,
+            write_chunks=True,
         )
-        simple_zarr_array[0, 0] = 1
-        simple_zarr_array[0, 1] = 2
-        simple_zarr_array[7, 7] = 3
 
     # nested
     # TODO: Use latest zarr-python once https://github.com/zarr-developers/zarr-python/pull/716 is merged
@@ -62,19 +59,9 @@ def generate_fixtures():
         key_separator="/",
         auto_mkdir=True,
     )
-
-    nested = zarr.open(
-        store,
-        shape=(8, 8),
-        chunks=(2, None),
-        dtype=">i4",
-        fill_value=0,
-        mode="w",
+    create_simple_array(
+        store=store, dtype=">i4", compression="blosc", write_chunks=True
     )
-    nested[0, 0] = 1
-    nested[0, 1] = 2
-    nested[7, 7] = 3
-
     # Manually add dimension separator to array meta
     meta = json_loads(store[".zarray"])
     meta["dimension_separator"] = "/"
