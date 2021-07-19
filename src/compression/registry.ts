5df3724eded5a9b0158b81d61a26ce0c5ef263b7
Trevor Manz <trevor.j.manz@gmail.com>
2021-07-19 12:53:52 -0400

    Fix numcodecs types (#97)

--- src/compression/registry.ts
+++ src/compression/registry.ts
@@ -1,18 +1,7 @@
-import type { Codec, CompressorConfig } from 'numcodecs';
+import type { Codec, CodecConstructor } from 'numcodecs';
 
-// TODO: This interface is tied to compressors in numcodecs..
-// might be better to just use 'any' or have numcodecs export complete 
-// (optional) config? 
-interface Options {
-  level?: number;
-  cname?: string;
-  blocksize?: number;
-  clevel?: number;
-  shuffle?: number;
-}
-
-type CodecConstructor = { fromConfig(config: Options & CompressorConfig): Codec };
-type CodecImporter = () => CodecConstructor | Promise<CodecConstructor>;
+type Config = Record<string, unknown>;
+type CodecImporter = () => CodecConstructor<Config> | Promise<CodecConstructor<Config>>;
 
 const registry: Map<string, CodecImporter> = new Map();
 
@@ -20,10 +9,10 @@ export function addCodec(id: string, importFn: CodecImporter) {
   registry.set(id, importFn);
 }
 
-export async function getCodec<T extends Codec>(config: Options & CompressorConfig): Promise<T> {
+export async function getCodec(config: Config & { id: string }): Promise<Codec> {
   if (!registry.has(config.id)) {
     throw new Error(`Compression codec ${config.id} is not supported by Zarr.js yet.`);
   }
-  const codec = await (registry.get(config.id) as CodecImporter)();
-  return codec.fromConfig(config) as T;
+  const codec = await registry.get(config.id)!();
+  return codec.fromConfig(config);
 }
